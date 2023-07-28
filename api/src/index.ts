e68d1c43259cce3b3543d061e59194d5a0ff1de5
Mateusz Kłakus <mati.klakus@gmail.com>
2023-07-28 12:24:00 +0200

    backend/feat moved init function to /utils

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,13 @@
-import express, { Express } from 'express';
 import { config as dotenvConfig } from 'dotenv';
-import routes from './routes';
+import { init } from './utils/init';
 
 dotenvConfig();
 
 const _PORT_ = process.env.PORT || 3001;
 
-function createApp(): Express {
-  const app = express();
-  app.use('/', routes);
-  return app;
-}
-
 async function main() {
   try {
-    const app = createApp();
+    const app = init();
     app.listen(_PORT_, () => {
       console.log(`Server running on port ${_PORT_}`);
     });
